45f9e43189212cf65a354fe3ddfa5b39cd03461c
Fernando "Brujo" Benavides <greenmellon@gmail.com>
2011-07-09 11:52:20 -0300

    syncing again...

--- src/edit_document.erl
+++ src/edit_document.erl
@@ -13,7 +13,7 @@
 -behaviour(gen_server).
 
 -export([create/0, start_link/1]).
--export([event_dispatcher/2, process_name/2]).
+-export([event_dispatcher/1, process_name/1, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -include("elog.hrl").
