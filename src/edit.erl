bb27e56ea411af72e557e5d97b098039e43f84b9
C B DePue <chad@inakanetworks.com>
2011-07-08 23:10:50 -0300

    app booting does nothing

--- src/edit.erl
+++ src/edit.erl
@@ -1,23 +1,25 @@
-%%%'   HEADER
 %%% @author C B DePue <chad@inakanetworks.com> 
 %%% @since 
 %%% @copyright 2011 C B DePue
 %%% @doc 
 %%% @end
 
--module(edit_app).
+-module(edit).
 -behaviour(application).
 
+-export([start/0, stop/0]).
 -export([start/2, stop/1]).
-%%%.
-%%%'   CALLBACKS
+
+start() ->
+  application:start(public_key),
+  application:start(ssl),
+  application:start(edit).
+
+stop() -> 
+  application:stop(edit).
+
 start(_StartType, _StartArgs) ->
   edit_sup:start_link().
 
 stop(_State) ->
   ok.
-%%%.
-%%%'   PRIVATE FUNCTIONS
-
-%%%.
-%%% vim: set filetype=erlang tabstop=2 foldmarker=%%%',%%%. foldmethod=marker:
