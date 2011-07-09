405f8cafc8797f14082e55c8cba2665f5c92f71e
Manuel Gomez <manuel@inakanetworks.com>
2011-07-08 23:24:42 -0300

    added diff match patch

--- www/js/application.js
+++ www/js/application.js
@@ -1,17 +1,30 @@
 var TypeSocial = {
   editor: null,
+  socket:null,
+  current_user:null,
+  user_list:null,
+  dmp:new diff_match_patch(),
+  patch: function(diff_object) {
+
+  },
+  localtest:function(text)
+  {
+    
+  },
   config: {
     toolbar:'Basic',
     resize_enabled:false,
     height:400,
     width:400
   },
-  current_user:null,
-  user_list:null,
-  onReady: function(){console.log("It's TypeSocialed!");},
+  login: function(){},
+  onEditorReady: function(){console.log("It's TypeSocialed!");},
   init: function(ext_config) {
+    // Set up Socket.io
+    
+    // Create the editor and set the config vars
     if (ext_config) {this.config = config}
-    this.editor = $('#editor').ckeditor(this.onReady,this.config); 
+    this.editor = $('#editor').ckeditor(this.onEditorReady,this.config); 
   }
 }
 
