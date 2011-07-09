df1aecc55288e980380d15fd48ab773881872519
Manuel Gomez <manuel@inakanetworks.com>
2011-07-08 23:00:47 -0300

    creating simple editor

--- www/js/application.js
+++ www/js/application.js
@@ -1,12 +1,23 @@
-var Weedit = {
-  init: function() {
-    $('#editor').ckeditor(); 
+var TypeSocial = {
+  editor: null,
+  config: {
+    toolbar:'Basic',
+    resize_enabled:false,
+    height:400,
+    width:400
+  },
+  current_user:null,
+  user_list:null,
+  onReady: function(){console.log("It's TypeSocialed!");},
+  init: function(ext_config) {
+    if (ext_config) {this.config = config}
+    this.editor = $('#editor').ckeditor(this.onReady,this.config); 
   }
 }
 
 
 $(document).ready(function(){
 
-  Weedit.init();
+  TypeSocial.init();
 
 });
