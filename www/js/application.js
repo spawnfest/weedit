0c5453411f50450ab44a10175e80510e99e490c4
Manuel Gomez <manuel@inakanetworks.com>
2011-07-09 12:56:39 -0300

    added methods for title sync

--- www/js/application.js
+++ www/js/application.js
@@ -3,13 +3,11 @@ var DiffMatchPatch = {
   init: function() {
     this.object = new diff_match_patch();
   },
-  applyPatch: function(text1,text2,diff_object) {
+  applyPatch: function(text1,diff_object) {
     console.log("Patch");
     console.log("Text1 = " + text1);
-    console.log("Text2 = " + text2);
-    console.log("Diff = " + diff_object);
 
-    patches = this.object.patch_make(text2, diff_object)
+    patches = this.object.patch_make(text1,diff_object)
 
     console.log("Patches = " + patches);
 
@@ -20,10 +18,11 @@ var DiffMatchPatch = {
   },
   getDiff: function(text1,text2) {
     diff = this.object.diff_main(text1, text2, true); 
-    console.log("Diff = " + diff)
     if (diff.length > 2) {
       this.object.diff_cleanupSemantic(diff);
     }
+
+    console.log("Diff = " + diff);
     return diff;
   }
 }
@@ -32,27 +31,33 @@ var TSocket = {
   object:null,
   init: function(hostname,port) {
     this.object = new io.Socket(hostname, {port:port});
+
+    this.object.on('connect', function(){
+      console.log("We connected!!");
+    });
   },
   doLogin: function(id,username) { 
     console.log("id and username = " + [id,username]); 
     //this.object.send('action':'login');
   },
-  setTitle: function(title) { console.log(title); }
+  setTitle: function(diff) { 
+    console.log(diff); 
+    this.object.send({"action":"setTitle","diff":diff});
+  }
 }
-
 
 var TypeSocial = {
   editor: null,
+  title: null,
   socket: null,
   current_user: null,
   user_list: null,
   dmp: DiffMatchPatch,
-  localtest:function(text) {
+  TestTitleChange: function(text) {
     console.log("Local Text");
-    console.log("Text = " + text)
-
-    diff = this.dmp.getDiff(this.editor.val(), text); 
-    this.editor.val(this.dmp.applyPatch(this.editor.val(),text,diff));
+    console.log("Text = " + text);
+    diff = this.dmp.getDiff(this.title.val(),text);
+    this.title.val(this.dmp.applyPatch(this.title.val(),diff));
   },
   socket: TSocket,
   config: {
@@ -69,17 +74,26 @@ var TypeSocial = {
       console.log("Here is the data" + event);
     });
   },
+  receiveTitle: function(diff) {
+    this.title.val(this.dmp.applyPatch(this.title.val(),diff));       
+  },
   init: function(ext_config) {
+    // Create the editor and set the config vars
+    if (ext_config) {this.config = config}
+    this.editor = $('#editor').ckeditor(this.onEditorReady,this.config); 
+
+    this.title = $('#document_title');
+
     // Set up Diff Match Patch
     this.dmp.init();
 
     // Set up Socket.io
     this.socket.init(location.hostname,'12000');
-     
-    // Create the editor and set the config vars
-    if (ext_config) {this.config = config}
-    this.editor = $('#editor').ckeditor(this.onEditorReady,this.config); 
-    
+
+    this.socket.object.on('newtitle', function(data){
+      console.log(data);
+      TypeSocial.receiveTitle(data.diff);
+    });
   }
 }
 
